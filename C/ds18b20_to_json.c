17e8a44dbcdb4da2e4510084fe9947a94cf38f1a
Hank Barta <hbarta@gmail.com>
2024-04-26 21:05:05 -0500

    presence, length cmd line arg

--- C/ds18b20_to_json.c
+++ C/ds18b20_to_json.c
@@ -1,10 +1,10 @@
 /*
 Read temperature from DS18B20 sensor, convert to °F and
-write to STDOUT in JSON format. 
+write to STDOUT in JSON format.
 See https://github.com/HankB/DS18B20-to-JSON/blob/main/README.md
 for more detail.
 
-Command line arguments 
+Command line arguments
     ds18B20 handle. Any of the values matching '/sys/bus/w1/devices/28*'
 
 Build using
@@ -12,8 +12,25 @@ Build using
 */
 
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char** argv)
+static const uint id_length = 15; // typical 28-3c01b607c935
+static const char *sensor_path = "./test/";
+
+void usage(char *progname, int error)
 {
-    printf("hello world\n");
+    fprintf(stderr, "Usage, %s [DS18B20 ID - required]\n", progname);
+    exit(error);
+}
+
+int main(int argc, char **argv)
+{
+    if ((argc != 2) ||                  // DS18B20 ID provided?
+        (strlen(argv[1]) != id_length)) // right length?
+    {
+        usage(argv[0], EINVAL);
+    }
+    exit(0);
 }
