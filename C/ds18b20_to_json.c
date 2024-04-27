9a493492d86489b28b03a95b3dc448b37261632c
Hank Barta <hbarta@gmail.com>
2024-04-26 23:24:03 -0500

    construct file path

--- C/ds18b20_to_json.c
+++ C/ds18b20_to_json.c
@@ -16,8 +16,8 @@ Build using
 #include <stdlib.h>
 #include <string.h>
 
-static const uint id_length = 15; // typical 28-3c01b607c935
-static const char *sensor_path = "./test/";
+static const uint id_length = 15;           // typical 28-3c01b607c935
+static const char *sensor_path = "./test/"; // for testing
 
 void usage(char *progname, int error)
 {
@@ -32,5 +32,18 @@ int main(int argc, char **argv)
     {
         usage(argv[0], EINVAL);
     }
+
+    // construct full file path
+    uint buf_len = strlen(sensor_path)+id_length+1;
+    char *path_buf = malloc(buf_len);
+    if(path_buf == 0)
+    {
+        fprintf(stderr, "can't malloc() memory\n");
+        exit(ENOMEM);
+    }
+    path_buf[0] = 0; // null termiante empty string
+    strncpy(path_buf, sensor_path, buf_len);
+    strncat(path_buf, argv[1], id_length);
+    printf("\"%s\"\n", path_buf);
     exit(0);
 }
